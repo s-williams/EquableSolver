6323093d098b8c7d2c5afcd1b8d300ae93a400fe
S Williams <scottwill@tesco.net>
2016-12-17 20:41:06 +0000

    initial commit

---
+++ src/Config.java
@@ -0,0 +1,29 @@
+/**
+ * Stores the values of each of the pieces and the make up of the hand
+ */
+public class Config {
+
+    public final int WHITES_PER_HAND = 7;
+    public final int BLUES_PER_HAND = 2;
+
+    public final int ONE_SCORE = 1;
+    public final int TWO_SCORE = 2;
+    public final int THREE_SCORE = 3;
+    public final int FOUR_SCORE = 3;
+    public final int FIVE_SCORE = 4;
+    public final int SIX_SCORE = 4;
+    public final int SEVEN_SCORE = 5;
+    public final int EIGHT_SCORE = 5;
+    public final int NINE_SCORE = 6;
+
+    public final int PLUS_SCORE = 2;
+    public final int MINUS_SCORE = 3;
+    public final int TIMES_SCORE = 4;
+    public final int DIVIDE_SCORE = 10;
+    public final int SLASH_SCORE = 15;
+    public final int DECIMAL_SCORE = 10;
+
+    public final int EQUALS_SCORE = 1;
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Main class
+ */
+public class Main {
+
+//    http://i.ebayimg.com/images/g/7I0AAOSwAuNW4hiu/s-l300.jpg
+//    http://i.imgur.com/pEBaY9m.jpg
+}
